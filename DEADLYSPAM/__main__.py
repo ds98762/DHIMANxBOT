68d31744f7465be010514fcc86ae116b83c90655
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-17 22:40:40 +0530

    Create __main__.py

---
+++ DEADLYSPAM/__main__.py
@@ -0,0 +1,96 @@
+# Copyright © 2023-2024 by piroxpower@Github, < https://github.com/piroxpower >.
+#
+# This file is part of < https://github.com/Team-Deadly/DEADLY-SPAM-USERBOT > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Team-Deadly/DEADLY-SPAM-USERBOT/blob/main/LICENSE >
+#
+# All rights reserved ®.
+
+
+import os
+from pyrogram import Client, idle
+from telethon import TelegramClient
+from config import STRING1, STRING2, STRING3, STRING4, STRING5
+from DEADLYSPAM import cli1, cli2, cli3, cli4, cli5
+
+
+# STRING 1
+
+if STRING1.endswith("=") 
+  try:
+      cli1.run_until_disconnected() 
+  except Exception as e:
+      pass
+else:
+    try:
+        cli1.start()
+    except Exception as e:
+        pass  
+else:
+    print("[INFO] Client 1 Skipped") 
+
+# STRING 2
+
+if STRING2.endswith("=") 
+  try:
+      cli2.run_until_disconnected() 
+  except Exception as e:
+      pass
+else:
+    try:
+        cli2.start()
+    except Exception as e:
+        pass 
+ else:
+    print("[INFO] Client 2 Skipped") 
+
+# STRING 3
+
+if STRING3.endswith("=") 
+  try:
+      cli3.run_until_disconnected() 
+  except Exception as e:
+      pass
+else:
+    try:
+        cli3.start()
+    except Exception as e:
+        pass 
+else:
+    print("[INFO] Client 3 Skipped") 
+
+# STRING 4
+
+if STRING4.endswith("=") 
+  try:
+      cli4.run_until_disconnected() 
+  except Exception as e:
+      pass
+else:
+    try:
+        cli4.start()
+    except Exception as e:
+        pass  
+else:
+    print("[INFO] Client 4 Skipped") 
+
+# STRING 5
+
+if STRING5.endswith("=") 
+  try:
+      cli5.run_until_disconnected() 
+  except Exception as e:
+      pass
+else:
+    try:
+        cli5.start()
+    except Exception as e:
+        pass 
+ 
+else:
+    print("[INFO] Client 5 Skipped") 
+    
+print("Your DeadlySpam Userbot Deployed Successfully 🥳 Please visit @deadly_spam_bot for any help!") 
+idle() 
+
+    
